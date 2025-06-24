f8dfb64562e486bac61caafc8184ef3de6ba7ad4
khinewin <khinewin123@gmail.com>
2025-06-24 11:33:37 +0630

    fetch data

--- app/Livewire/Welcome.php
+++ app/Livewire/Welcome.php
@@ -3,9 +3,13 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Post;
+use Livewire\WithPagination;
 
 class Welcome extends Component
 {
+     use WithPagination;
+      protected $paginationTheme = 'bootstrap';
     public $title="", $content="";
 
     public function savePost(){
@@ -13,10 +17,22 @@ class Welcome extends Component
             "title"=>"required|min:5",
             "content"=>"required|min:5"
         ]);
+
+        $post=new Post();
+        $post->title=$this->title;
+        $post->content=$this->content;
+        $post->save();
+
+        $this->title="";
+        $this->content="";
+
+        session()->flash("success_msg", "The new post  has been created");
+
     }
 
     public function render()
     {
-        return view('livewire.welcome');
+        $posts=Post::OrderBy("id", "desc")->paginate(2);
+        return view('livewire.welcome')->with(["posts"=>$posts]);
     }
 }
--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -1,6 +1,9 @@
 <div class="container">
     <h2>Livewire Example</h2>
     <div class="row">
+        @session("success_msg")
+            <div class="alert alert-success">{{ session("success_msg") }}</div>
+        @endsession
         <div class="col-sm-4">
             <div class="card">
                 <div class="card-body">
@@ -26,6 +29,38 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-8"></div>
+        <div class="col-sm-8">
+            <div class="card">
+                <div class="card-body">
+                   <ul class="list-group">
+                        @foreach ($posts as  $p)
+                            <li class="list-group-item">
+                                    <div class="row">
+                                        <div class="col-1">
+                                            <div class="small text-lighter">ID</div>
+                                            <div class="fw-bold">{{ $p->id }}</div>
+                                        </div>
+                                        <div class="col-4">
+                                            <div class="smll text-lighter">Title</div>
+                                            <div class="fw-bold">{{ $p->title }}</div>
+                                        </div>
+                                         <div class="col-5">
+                                            <div class="smll text-lighter">Content</div>
+                                            <div class="fw-bold">{{ $p->content }}</div>
+                                        </div>
+                                        <div class="col-2">
+                                            <div class="flex">
+                                                <a href="#!" class="btn btn-secondary btn-sm">Edit</a>
+                                                <a href="#!" class="btn btn-danger btn-sm">Delete</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </li>
+                        @endforeach
+                   </ul>
+                   {{ $posts->links() }}
+                </div>
+            </div>
+        </div>
     </div>
 </div>
